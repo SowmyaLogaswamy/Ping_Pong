ae9900c6d352255d40e7ce32102b3a9fafdd8c84
Sowmya Logaswamy <sowmyalogaswamy@gmail.com>
2017-06-14 01:02:05 -0700

    Separate business logic from UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,30 @@
 //Business Logic
+var getNumToBeDisplayed = function(index)
+{
+  if(index % 15 === 0) {
+     return "pingpong";
+ }
+ else if(index % 3 === 0) {
+   return "ping";
+ }
+ else if(index % 5 === 0) {
+     return "pong";
+ }
+ else{
+   return index;
+ }
+}
 
 //User Interface Logic
 $(document).ready(function() {
   $("form#pingpong-form").submit(function(event) {
     event.preventDefault();
     var getNumber=parseInt($("#new-number").val());
-    // result = Play(getNumber);
       $("#new-number").text("");
     for(var index=1; index<=getNumber; index++) {
-       if(index % 15 === 0) {
-          $("p#result").append("pingpong");
-      }
-      else if(index % 3 === 0) {
-        $("p#result").append("ping");
-      }
-      else if(index % 5 === 0) {
-          $("p#result").append("pong");
-      }
-
-      else{
-        $("p#result").append(index);
-      }
+      $("p#result").append(getNumToBeDisplayed(index));
       $("p#result").append("<br>");
-
-    }
-
+  }
   });
 
 });
